95a5104c1acf1ae52b1aa1499c0db782b08a0767
yclv <lvyanchi@foxmail.com>
2017-09-30 14:52:04 +0800

    替换为向右增加一位的字母的算法

--- src/cn/yclv/crypto/security/KaisaiEncry.java
+++ src/cn/yclv/crypto/security/KaisaiEncry.java
@@ -3,10 +3,16 @@ package cn.yclv.crypto.security;
 public class KaisaiEncry {
 	public static void main(String[] args) {
 		String cyperText = "EXXEGOEXSRGI";
-//		stringToASCII(cyperText);
-		int[] a = new int[]{88,99};
-		String asciiToString = asciiToString(a);
+		System.out.println(cyperText);
+		int[] asciiArray = stringToASCII(cyperText);
+		for (int i = 0; i < asciiArray.length; i++) {
+			System.out.print(asciiArray[i] + "|");
+		}
+		int[] assciiToLeftOneStep = assciiToLeftOneStep(asciiArray);
+		String asciiToString = asciiToString(asciiArray);
+		String asciiToString2 = asciiToString(assciiToLeftOneStep);
 		System.out.println(asciiToString);
+		System.out.println(asciiToString2);
 	}
 
 	
@@ -25,7 +31,11 @@ public class KaisaiEncry {
 	}
 	
 	
-	
+	/**
+	 * ascii码转为String
+	 * @param asciiArray
+	 * @return
+	 */
 	private static String asciiToString(int[] asciiArray){
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < asciiArray.length; i++) {
@@ -33,5 +43,14 @@ public class KaisaiEncry {
 			sb.append(c);
 		}
 		return sb.toString();
+	}
+	
+	
+	private static int[] assciiToLeftOneStep(int[] asciiArray){
+		int[] toLeftOneStep = new int[12];
+		for (int i = 0; i < asciiArray.length; i++) {
+			toLeftOneStep[i] = asciiArray[i] + 1;
+		}
+		return toLeftOneStep;
 	}
 }
